6bc56690432674938a9ee6081d58f4390a1ead98
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-25 05:46:08 -0300

    join fetch

--- src/main/java/com/devsuperior/aulalazy/repositories/EmployeeRepository.java
+++ src/main/java/com/devsuperior/aulalazy/repositories/EmployeeRepository.java
@@ -9,6 +9,25 @@ import com.devsuperior.aulalazy.entities.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+	// CLAUSULA JOIN FETCH - PERFORMANCE
+	// HIBERNATE FAZ APENAS 1 CONSULTA NO BANCO
+	// NAO FUNCIONA COM BUSCA PAGINADA
 	@Query("SELECT obj FROM Employee obj JOIN FETCH obj.department")
 	List<Employee> findEmployeesWithDepartments();
+
+	/*
+	 * Hibernate:
+	 * select
+	 * e1_0.id,
+	 * d1_0.id,
+	 * d1_0.name,
+	 * e1_0.email,
+	 * e1_0.name
+	 * from
+	 * tb_employee e1_0
+	 * join
+	 * tb_department d1_0
+	 * on d1_0.id=e1_0.department_id
+	 * 
+	 */
 }
